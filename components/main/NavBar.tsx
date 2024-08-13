ec02344357652a9521539573ef02c1abad004e78
Gabriell Maia <88511880+MA1A01@users.noreply.github.com>
2024-08-12 22:39:17 -0300

    changes at the navbar

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -2,9 +2,9 @@ import { Socials } from "@/constant";
 import Image from "next/image";
 import React from "react";
 
-const NavBar = () => {
+const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50  bg-[#03001417] backdrop-blur-md z-50 px-10">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a
           href="#about-me"
@@ -17,12 +17,14 @@ const NavBar = () => {
             height={70}
             className="cursor-pointer hover:animate-slowspin"
           />
-          <span className="font-bold ml-[10px] hidden md:block text-gray-300 ">
+
+          <span className="font-bold ml-[10px] hidden md:block text-gray-300">
             Gabriell Maia
           </span>
         </a>
+
         <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between border w-full h-auto border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
             <a href="#about-me" className="cursor-pointer">
               About me
             </a>
@@ -33,21 +35,22 @@ const NavBar = () => {
               Projects
             </a>
           </div>
-          <div className="flex flex-row gap-5">
-            {Socials.map((social) => (
-              <Image
-                src={social.src}
-                alt={social.name}
-                key={social.name}
-                width={24}
-                height={24}
-              />
-            ))}
-          </div>
+        </div>
+
+        <div className="flex flex-row gap-5">
+          {Socials.map((social) => (
+            <Image
+              src={social.src}
+              alt={social.name}
+              key={social.name}
+              width={24}
+              height={24}
+            />
+          ))}
         </div>
       </div>
     </div>
   );
 };
 
-export default NavBar;
+export default Navbar;
