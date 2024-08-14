cc054abb133c74290485bdd45b82e79cc115d96e
Gabriell Maia <88511880+MA1A01@users.noreply.github.com>
2024-08-14 12:46:11 -0300

    att code

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -9,6 +9,23 @@ import React from "react";
 import SkillDataProvider from "../sub/SkillDataProvider";
 import SkillText from "../sub/SkillText";
 
+//Function criada para reduzir a duplicacao do codigo
+const renderSkillSection = (skillArray) => {
+  return (
+    <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+      {skillArray.map((image, index) => (
+        <SkillDataProvider
+          key={index}
+          src={image.Image}
+          width={image.width}
+          height={image.height}
+          index={index}
+        />
+      ))}
+    </div>
+  );
+};
+
 const Skills = () => {
   return (
     <section
@@ -16,61 +33,11 @@ const Skills = () => {
       style={{ transform: "scale(0.9)" }}
     >
       <SkillText />
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Frontend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
+      {renderSkillSection(Skill_data)}
+      {renderSkillSection(Frontend_skill)}
+      {renderSkillSection(Backend_skill)}
+      {renderSkillSection(Full_stack)}
+      {renderSkillSection(Other_skill)}
       <div className="w-full h-full absolute">
         <div className="w-full h-full z-[10px] opacity-30 absolute flex items-center justify-center bg-cover">
           <video
